510c767ac49db4cc622422b79f14135f0fdeb2e4
Duy Dinh <dduy07037@gmail.com>
2024-05-31 16:36:51 +0700

    deloy

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Layout from './component/LayoutExx/Layout';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Layout />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/LayoutExx/Layout.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Layheader from './Layheader'
+
+export default class Layout extends Component {
+  render() {
+    return (
+      <div>
+        <Layheader />
+      </div>
+    )
+  }
+}
