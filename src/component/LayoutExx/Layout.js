05f4ca6a4caa4c53eb9947039a9ef53eca831daf
Duy Dinh <dduy07037@gmail.com>
2024-06-01 12:35:27 +0700

    upload

--- src/component/LayoutExx/Layout.js
+++ src/component/LayoutExx/Layout.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react'
 import Layheader from './Layheader'
+import Navigation from './Navigation'
+import Content from './Content'
+import Footer from './Footer'
 
 export default class Layout extends Component {
   render() {
+
+    var noSp = {
+      margin:0,
+      padding:0,
+    }
+
     return (
-      <div>
-        <Layheader />
+      <div className='container-fluid'>
+        <div className='row'>
+            <div className='col-12 bg-dark'>
+              <Layheader />
+            </div>
+        </div>
+        <div className='row'>
+            <div style={noSp} className='col-4'>
+              <Navigation />
+            </div>
+            <div style={noSp} className='col-8'>
+              
+              <Content />
+            </div>
+        </div>
+        <div className='row'>
+            <div style={noSp} className='col-12 bg-warning-subtle'>
+              
+              <Footer />
+            </div>
+        </div>
       </div>
     )
   }
